3bd297b4aa09619a940383a3de61e3e32eff4ca4
dmcguQUB <dallanmcguckian@hotmail.com>
2023-07-10 21:16:43 +0100

    created changes to the direct ophthalmoscopy test updating navbar and code to work as intended

--- frontend/src/app/components/pages/direct-ophthalmoscopy-test/direct-ophthalmoscopy-test.component.ts
+++ frontend/src/app/components/pages/direct-ophthalmoscopy-test/direct-ophthalmoscopy-test.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
 import { Renderer2, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { CaseStudies } from 'src/app/shared/models/casestudies'; // Assuming the model is placed here
 
 @Component({
   selector: 'app-direct-ophthalmoscopy-test',
@@ -11,6 +12,8 @@ export class DirectOphthalmoscopyTestComponent implements OnInit, AfterViewInit 
   // Obtain a reference to the canvas element
   @ViewChild('myCanvas', { static: true })
   canvas!: ElementRef<HTMLCanvasElement>;  
+
+  @Input() caseStudy!: CaseStudies;
 
   constructor(private renderer2: Renderer2, @Inject(DOCUMENT) private _document: Document) { }
 
