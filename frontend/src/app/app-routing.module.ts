82f82895ce300cd172fc5c19b64a3ebca13fab3b
dmcguQUB <dallanmcguckian@hotmail.com>
2023-08-17 19:14:48 +0100

    made changes to the application structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { CaseStudyDetailComponent } from './components/pages/case-study-detail/case-study-detail.component'; // Adjust the path as needed
-import { PupilReflexesTestComponent } from './components/pages/pupil-reflexes-test/pupil-reflexes-test.component';
-import { VisualFieldsTestLeftComponent } from './components/pages/visual-fields-test-left/visual-fields-test-left.component'; // visual fields test by confrontation imported route
-import { VisualAcuityTestComponent } from './components/pages/visual-acuity-test/visual-acuity-test.component'; //visual acuity test
-import { EyeMovementsTestComponent } from './components/pages/eye-movements-test/eye-movements-test.component'; //eye movemebts
-import { DirectOphthalmoscopyTestComponent } from './components/pages/direct-ophthalmoscopy-test/direct-ophthalmoscopy-test.component';
-import { QuestionComponent } from './components/pages/question/question.component';
-import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { PupilReflexesTestComponent } from './components/pages/eye-tests/pupil-reflexes-test/pupil-reflexes-test.component';
+import { VisualFieldsTestLeftComponent } from './components/pages/eye-tests/visual-fields-test-left/visual-fields-test-left.component'; // visual fields test by confrontation imported route
+import { VisualAcuityTestComponent } from './components/pages/eye-tests/visual-acuity-test/visual-acuity-test.component'; //visual acuity test
+import { EyeMovementsTestComponent } from './components/pages/eye-tests/eye-movements-test/eye-movements-test.component'; //eye movemebts
+import { DirectOphthalmoscopyTestComponent } from './components/pages/eye-tests/direct-ophthalmoscopy-test/direct-ophthalmoscopy-test.component';
+import { QuestionComponent } from './components/pages/QA/question/question.component';
+import { WelcomeComponent } from './components/pages/QA/welcome/welcome.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
-import { ScoresComponent } from './components/pages/scores/scores.component';
-import { AdminPageComponent } from './components/pages/admin-page/admin-page.component';
-import { VisualFieldsTestRightComponent } from './components/pages/visual-fields-test-right/visual-fields-test-right.component';
-import { AdminAvgScoreVsTimeComponent } from './components/pages/admin-avg-score-vs-time/admin-avg-score-vs-time.component';
-import { AdminRegistrationsOverTimeComponent } from './components/pages/admin-registrations-over-time/admin-registrations-over-time.component';
+import { ScoresComponent } from './components/pages/graphs/scores/scores.component';
+import { AdminPageComponent } from './components/pages/graphs/admin-page/admin-page.component';
+import { VisualFieldsTestRightComponent } from './components/pages/eye-tests/visual-fields-test-right/visual-fields-test-right.component';
+import { AdminAvgScoreVsTimeComponent } from './components/pages/graphs/admin-avg-score-vs-time/admin-avg-score-vs-time.component';
+import { AdminRegistrationsOverTimeComponent } from './components/pages/graphs/admin-registrations-over-time/admin-registrations-over-time.component';
 import { AuthGuard } from './auth/guards/auth.guard';
 import { AdminGuard } from './auth/guards/admin.guard';
-import { TestVisualFieldsTestComponent } from './components/pages/test-visual-fields-test/test-visual-fields-test.component';
-import { UserscoreScoreOverTimeComponent } from './components/pages/userscore-score-over-time/userscore-score-over-time.component';
+import { TestVisualFieldsTestComponent } from './components/pages/eye-tests/test-visual-fields-test/test-visual-fields-test.component';
+import { UserscoreScoreOverTimeComponent } from './components/pages/graphs/userscore-score-over-time/userscore-score-over-time.component';
 import { UserProfilePageComponent } from './components/pages/user-profile-page/user-profile-page.component';
 
 const routes: Routes = [
