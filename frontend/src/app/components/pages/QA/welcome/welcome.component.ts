9545f046990a85a65ab798b5e4af3c11ccdbf98c
dmcguQUB <dallanmcguckian@hotmail.com>
2023-08-22 14:50:35 +0100

    all questions - improved performance

--- frontend/src/app/components/pages/QA/welcome/welcome.component.ts
+++ frontend/src/app/components/pages/QA/welcome/welcome.component.ts
@@ -1,33 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'; // Import OnDestroy
 import { ActivatedRoute, Router } from '@angular/router';
 import { QuestionService } from 'src/app/services/questions.service'; // Import your service
+import { Subscription } from 'rxjs'; // Import Subscription
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, OnDestroy {
+  // Implement OnDestroy
   user: any;
   useCaseId: any;
-  loading: boolean = true; 
-  totalQuestions?: number;  // Define totalQuestions variable
+  loading: boolean = true;
+  totalQuestions?: number; // Define totalQuestions variable
 
-  constructor(private router: Router, private route: ActivatedRoute, private questionService: QuestionService){} // Inject your service
+  private subscription?: Subscription; // Keep track of subscription
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private questionService: QuestionService
+  ) {} // Inject your service
 
   ngOnInit(): void {
     this.useCaseId = this.route.snapshot.paramMap.get('useCaseId');
-    this.getQuizDetails();  // Fetch quiz details
+    this.getQuizDetails(); // Fetch quiz details
   }
 
   getQuizDetails() {
-    this.questionService.getQuestionsByCaseStudyId(this.useCaseId).subscribe((questions) => {
-      this.totalQuestions = questions.length;
-      this.loading = false;
-    });
+    this.questionService
+      .getQuestionsByCaseStudyId(this.useCaseId)
+      .subscribe((questions) => {
+        this.totalQuestions = questions.length;
+        this.loading = false;
+      });
   }
 
   startQuiz() {
     this.router.navigate(['/eye-test-questions', this.useCaseId]);
+  }
+
+  ngOnDestroy(): void {
+    // OnDestroy lifecycle hook to unsubscribe from the observable
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
