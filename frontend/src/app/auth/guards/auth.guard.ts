b4f4b19966031b75c505680cf4e47944e111658b
dmcguQUB <dallanmcguckian@hotmail.com>
2023-08-22 16:56:51 +0100

    auth - improved performance

--- frontend/src/app/auth/guards/auth.guard.ts
+++ frontend/src/app/auth/guards/auth.guard.ts
@@ -7,14 +7,17 @@ import {
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { UserService } from 'src/app/services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+
   constructor(private userService: UserService, private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -23,9 +26,26 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.userService.currentUser.token) return true;
 
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-    return false;
+    // Check if currentUser is an observable, and handle it. Otherwise, proceed with the initial logic.
+    if (typeof this.userService.currentUser === 'object' && this.userService.currentUser instanceof Observable) {
+      return this.userService.currentUser.pipe(
+        map(user => {
+          if (user.token) return true;
+
+          this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+          return false;
+        }),
+        catchError(() => {
+          this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+          return of(false);
+        })
+      );
+    } else {
+      if (this.userService.currentUser.token) return true;
+      
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+      return false;
+    }
   }
 }
