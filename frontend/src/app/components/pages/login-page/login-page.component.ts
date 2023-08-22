c76ab17394f2b6998fafda9c129a261f61052da5
dmcguQUB <dallanmcguckian@hotmail.com>
2023-08-22 14:10:12 +0100

    login changes - improve performance

--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'; // Add OnDestroy
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
+import { Subscription } from 'rxjs'; // Import Subscription for managing subscriptions
+
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy { // Implement OnDestroy
   //angular forms used
   loginForm!:FormGroup;
   //indicator if the user has submitted form yet. Set to false so errors don't show until form submitted
   isSubmitted = false;
-  
   returnUrl = '';
+  private subscription: Subscription = new Subscription(); // Create a Subscription object to hold our subscriptions
+
 
   //use Form builder from angular, inject user servuce
-  constructor(private formBuilder: FormBuilder
-    , private userService:UserService,
-     private activatedRoute:ActivatedRoute,
-     private router:Router) { }
+  constructor(
+    private formBuilder: FormBuilder, 
+    private userService:UserService,
+    private activatedRoute:ActivatedRoute,
+    private router:Router) { }
 
   ngOnInit(): void {
     //login form is used for the form builder
@@ -48,12 +52,19 @@ export class LoginPageComponent implements OnInit {
     if(this.loginForm.invalid) return;
 
     //send the email and password to the userservice
-    this.userService.login({email: this.fc['email'].value,
-    password: this.fc['password'].value}).subscribe(() => {
-         this.router.navigateByUrl(this.returnUrl);
+    const loginSub = this.userService.login({
+      email: this.fc['email'].value,
+      password: this.fc['password'].value
+    }).subscribe(() => {
+      this.router.navigateByUrl(this.returnUrl);
+    });
 
+    this.subscription.add(loginSub); // Add this subscription to our Subscription object
+  }
 
-       });
+  //destroy component after leaving component
+  ngOnDestroy(): void { // Implement ngOnDestroy lifecycle hook
+    this.subscription.unsubscribe(); // Unsubscribe from all subscriptions to prevent memory leaks
   }
 
 }
